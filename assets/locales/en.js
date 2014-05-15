340d8c757222f1a24a6ad0f7326ace4482e951f1
RobinQu <robinqu@gmail.com>
2014-05-15 13:31:37 +0800

    style fix

--- assets/locales/en.js
+++ assets/locales/en.js
@@ -1,4 +1,14 @@
 {
 	"Distributed, living spec runner": "Distributed, living spec runner",
-	"Learn more": "Learn more"
+	"Learn more": "Learn more",
+	"Test, distributed": "Test, distributed",
+	"Tests are run in the browser of every possible Internet users. You can share your test runner to all your targeting users directly. What's more, you can get real-time reports of your test results.": "Tests are run in the browser of every possible Internet users. You can share your test runner to all your targeting users directly. What's more, you can get real-time reports of your test results.",
+	"Integrated with Github repo": "Integrated with Github repo",
+	"Every Github repo can be configured to run the test files commited. Just write some rules in .webspec file and commit. Every commit will triggered a new sandbox for test runner. And tests can be runned against a specified revision.": "Every Github repo can be configured to run the test files commited. Just write some rules in .webspec file and commit. Every commit will triggered a new sandbox for test runner. And tests can be runned against a specified revision.",
+	"About .webspec file": "About .webspec file",
+	"Create a test suites": "Create a test suites",
+	"Write a group fo spec files online. It's suitable for quick demonstrations or proof of concepts when reporting a bug or doing some compatible experiments.": "Write a group fo spec files online. It's suitable for quick demonstrations or proof of concepts when reporting a bug or doing some compatible experiments.",
+	"Create one": "Create one",
+	"Integrated with Github repositories": "Integrated with Github repositories",
+	"Distributed, living spec runner. Crowd-sourcing your tests for borwser Javascript to all Internet users.": "Distributed, living spec runner. Crowd-sourcing your tests for borwser Javascript to all Internet users."
 }
